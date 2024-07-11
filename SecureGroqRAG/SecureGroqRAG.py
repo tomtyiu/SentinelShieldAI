62beb60d6979335b78a445002b952855020d71ba
Thomas YIu <tom.tyiu@gmail.com>
2024-07-11 00:54:23 -0700

    Update SecureGroqRAG.py

--- SecureGroqRAG/SecureGroqRAG.py
+++ SecureGroqRAG/SecureGroqRAG.py
@@ -7,7 +7,7 @@ import getpass
 import os
 
 
-os.environ["GROQ_API_KEY"] = "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"
+os.environ["GROQ_API_KEY"] = "spikes"
 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
