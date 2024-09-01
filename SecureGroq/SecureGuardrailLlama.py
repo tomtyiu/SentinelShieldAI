3397cc31ff21403e0cf211fbe617961aa86577c8
Thomas YIu <tom.tyiu@gmail.com>
2024-09-01 16:57:25 -0700

    Update SecureGuardrailLlama.py

--- SecureGroq/SecureGuardrailLlama.py
+++ SecureGroq/SecureGuardrailLlama.py
@@ -6,7 +6,7 @@ from llama_index.core.llms import ChatMessage
 from llama_index.llms.groq import Groq
 #Create an API key at the Groq console
 #You can pass your API key to the LLM when you init it:
-llm = Groq(model="llama-3.1-405b-reasoning", api_key="api key")
+llm = Groq(model="llama-3.1-70b-versatile", api_key="api key")
 # llm = Groq(model="llama-3.1-70b-versatile", api_key="api key")
 # llm = Groq(model="llama-3.1-8b-instant", api_key="api key")
 
